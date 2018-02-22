1cd7387e9c2c258765e19846aad12eaa7f116829
Cecile Camillieri <cecile.camillieri@gmail.com>
2018-02-22 14:55:28 +0100

    Link to source code in the pages

--- js/read-metadata.js
+++ js/read-metadata.js
@@ -1,16 +1,17 @@
 function metaLoaded(meta) {
-    document.getElementById("year-title").innerHTML += ' ' + meta.year;
-    document.getElementById("day-title").innerHTML += ', day ' + meta.day;
-    document.getElementById("puzzle-title").innerHTML = meta.title;
+    document.getElementById('year-title').innerHTML += ' ' + meta.year;
+    document.getElementById('main-title').innerHTML += ', day ' + meta.day;
+    document.getElementById('puzzle-title').innerHTML = meta.title;
+    document.getElementById('github-link').href += meta.year + '/day/' + meta.day;
 
-    document.getElementById("instructions-content").innerHTML = meta.instructions;
-    document.getElementById("puzzle-link").href = 'https://adventofcode.com/' + meta.year + '/day/' + meta.day;
+    document.getElementById('instructions-content').innerHTML = meta.instructions;
+    document.getElementById('puzzle-link').href = 'https://adventofcode.com/' + meta.year + '/day/' + meta.day;
 
     document.title = 'AoC ' + meta.year + ' - day ' + meta.day;
 }
 
 function loadMeta() {
-    $.getJSON("metadata.json", metaLoaded);
+    $.getJSON('metadata.json', metaLoaded);
 }
 
 document.onload = loadMeta();
